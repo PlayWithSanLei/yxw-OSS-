cdb1fa8396299c7cf13ac5fe901b79ffc4b9574b
hao zhang <zhanghao63271202@gmail.com>
2021-05-22 18:10:29 +0800

    删了没用的代码

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,7 +3,7 @@
     <form id="formId">
       <label>上传文档</label>
       <input type="file" @change="inputChange">
-      <el-button type="button" @click="put">asdasd</el-button>
+      <el-button type="button" @click="put">上传</el-button>
     </form>
   </div>
 
@@ -32,39 +32,18 @@ export default {
       fileReads.onload = function () {
         var wordArray = CryptoJs.lib.WordArray.create(fileReads.result)
         var hash = CryptoJs.SHA256(wordArray).toString()
-
-        console.log(hash)
-        console.log(wordArray)
-        console.log(files)
         var base64 = Base64.encode(hash)
         console.log(base64)
         window.sessionStorage.setItem('filename', files.name)
         window.sessionStorage.setItem('base64',base64)
-
         that.file = files
         that.filename = files.name
         that.base64 = base64
         that.Digest = 'SHA256='+base64
-        console.log(that.file)
-        console.log(that.filename)
-        console.log(that.base64)
       }
     },
     put() {
       const name = this.filename
-      // const put = this.$http.create({
-      //   baseURL: 'http://192.168.43.159:9426/api/v2/oss/objects/',
-      //   timeout:10000,
-      //   headers: {
-      //     'Digest': this.Digest,
-      //     "Access-Control-Allow-Origin": "*",
-      //     "Access-Control-Allow-Headers": "Content-Type,AccessToken,X-CSRF-Token, Authorization, Token",
-      //     "Access-Control-Allow-Methods": "PUT,POST,UPDATE,DELETE,GET,OPTIONS,HEAD",
-      //     "Access-Control-Expose-Headers": "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Content-Type",
-      //     "Access-Control-Allow-Credentials": "true"
-      //   }
-      // })
-      // put.put(name)
       console.log(this.Digest)
       this.$http.put('oss/objects/image-20210401214330182.png')
     }
